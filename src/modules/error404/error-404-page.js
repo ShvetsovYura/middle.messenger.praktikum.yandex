d36adabaa256e8e02abc2d05a517cb0ef5c92761
Shvetsov Yura <oygemor@yandex.ru>
2021-07-08 18:54:36 +0300

    fix handlebars error

--- src/modules/error404/error-404-page.js
+++ src/modules/error404/error-404-page.js
@@ -2,4 +2,4 @@ import error404PageTpl from "./error-404-page.tpl";
 const Handlebars = require("handlebars");
 
 const template = Handlebars.compile(error404PageTpl);
-document.body.innerHTML = "sdfdsf";
+document.body.innerHTML = template();
