e10868ba993f2da82dea57f33c565f94e2fd4a18
Shvetsov Yura <oygemor@yandex.ru>
2021-07-08 08:01:34 +0300

    change server run

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const Bundler = require("parcel-bundler");
 const express = require("express");
 
 const app = express();
 
-const file = "src/index.html";
+// const file = "src/index.html";
 
-const bundler = new Bundler(file, {});
+// const bundler = new Bundler(file, {});
 app.use(express.static("public"));
-app.use(bundler.middleware());
+app.use(express.static('dist'));
+// app.use(bundler.middleware());
 
 app.listen(3000, function () {
   console.log("server start");
