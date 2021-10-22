9e79f2a86d43f49e13b84fd64a0b1cecdb02a24f
ShvetsovYura <shvetsov.yura@hotmail.com>
2021-10-22 16:43:17 +0300

    fix handelbars registerHelper error

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { registerHelper } from 'handlebars';
 import ChangePasswordPage from './pages/change-password/change-password';
 import ErrorPage404 from './pages/error-page-404/index';
 import ErrorPage500 from './pages/error-page-500/index';
@@ -7,6 +8,15 @@ import ProfilePage from './pages/profile';
 import RegistrationPage from './pages/registeration/index';
 import Router from './services/router/router';
 import './styles/style.less';
+
+registerHelper('isdefined', (value) => !value);
+registerHelper('stringDateToTime', (dt: string) => {
+  const date = new Date(dt);
+  if (date) {
+    return date.toLocaleString();
+  }
+  return '';
+});
 
 const router = new Router('#app');
 
